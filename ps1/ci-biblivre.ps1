c6f685ab474220db16fd968dab1811af375fb290
nicholas <nicholas.pinto@ufes.br>
2025-07-30 15:03:03 -0300

    update p lab 708

--- ps1/ci-biblivre.ps1
+++ ps1/ci-biblivre.ps1
@@ -1,2 +1,8 @@
 # https://github.com/Biblivre-org/Biblivre5x/
+
+# install docker, ensure it uses hyper v backend
+# run docker desktop
+# "C:\Program Files\Docker\Docker\resources\bin\docker.exe" pull cleydyr/biblivre
+
+# alternativamente, rodar num unico servidor?
 Write-Host "test biblivre 5 install"

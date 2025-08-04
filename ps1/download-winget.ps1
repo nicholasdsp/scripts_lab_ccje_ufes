5837484bacf9eddebff9300a585c00669bf3595c
nicholas <nicholas.pinto@ufes.br>
2025-08-04 17:34:58 -0300

    improve download-winget, to remove dependencies from repo

--- ps1/download-winget.ps1
+++ ps1/download-winget.ps1
@@ -1,3 +1,12 @@
 $latestWingetMsixBundleUri = $(Invoke-RestMethod https://api.github.com/repos/microsoft/winget-cli/releases/latest).assets.browser_download_url | Where-Object {$_.EndsWith(".msixbundle")}
 Write-Information "downloading winget ..."
 Invoke-WebRequest -Uri $latestWingetMsixBundleUri -OutFile "./ps1/winget-dependencies/winget.msixbundle"
+#todo: check if dependencies exist and download if not
+if (-Not (Test-Path -Path "./ps1/winget-dependencies/Microsoft.UI.Xaml.2.8.appx"))
+{
+    Invoke-WebRequest -Uri "http://ccje-websrv1/repositorio/Microsoft.UI.Xaml.2.8.appx" -OutFile "./ps1/winget-dependencies/Microsoft.UI.Xaml.2.8.appx"
+}
+if (-Not (Test-Path -Path "./ps1/winget-dependencies\Microsoft.VCLibs.140.00.UWPDesktop_14.0.33728.0_x64__8wekyb3d8bbwe.appx"))
+{
+    Invoke-WebRequest -Uri "http://ccje-websrv1/repositorio/Microsoft.VCLibs.140.00.UWPDesktop_14.0.33728.0_x64__8wekyb3d8bbwe.Appx" -OutFile "./ps1/winget-dependencies/Microsoft.VCLibs.140.00.UWPDesktop_14.0.33728.0_x64__8wekyb3d8bbwe.appx"
+}

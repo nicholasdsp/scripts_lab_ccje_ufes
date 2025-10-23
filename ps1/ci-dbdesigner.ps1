84ca87cec8eb32150817abf4b9e7f13c4e7615a4
nicholas <nicholas.pinto@ufes.br>
2025-10-23 14:42:21 -0300

    repo url update

--- ps1/ci-dbdesigner.ps1
+++ ps1/ci-dbdesigner.ps1
@@ -1,3 +1,3 @@
 . '.\ps1\custom-installation.ps1'
 
-DoCustomInstall -url "http://ccje-websrv1/repositorio/DBDesigner4.0.5.6_Full.zip" -program_name "DBDesigner" -exec_filename "DBDesigner4.exe" -username "Aluno_CCJE"
+DoCustomInstall -url "http://ccje-web/lab/DBDesigner4.0.5.6_Full.zip" -program_name "DBDesigner" -exec_filename "DBDesigner4.exe" -username "Aluno_CCJE"
--- ps1/download-winget.ps1
+++ ps1/download-winget.ps1
@@ -3,16 +3,16 @@ if (-Not (Test-Path -Path "./ps1/winget-dependencies"))
     New-Item "./ps1/winget-dependencies" -ItemType Directory -ea 0
 }
 
-$latestWingetMsixBundleUri = "http://ccje-websrv1/repositorio/winget.msixbundle"
+$latestWingetMsixBundleUri = "http://ccje-web/lab/winget.msixbundle"
 #$latestWingetMsixBundleUri = $(Invoke-RestMethod https://api.github.com/repos/microsoft/winget-cli/releases/latest).assets.browser_download_url | Where-Object {$_.EndsWith(".msixbundle")}
 Write-Information "downloading winget ..."
 Invoke-WebRequest -Uri $latestWingetMsixBundleUri -OutFile "./ps1/winget-dependencies/winget.msixbundle"
 
 if (-Not (Test-Path -Path "./ps1/winget-dependencies/Microsoft.UI.Xaml.2.8.appx"))
 {
-    Invoke-WebRequest -Uri "http://ccje-websrv1/repositorio/Microsoft.UI.Xaml.2.8.appx" -OutFile "./ps1/winget-dependencies/Microsoft.UI.Xaml.2.8.appx"
+    Invoke-WebRequest -Uri "http://ccje-web/lab/Microsoft.UI.Xaml.2.8.appx" -OutFile "./ps1/winget-dependencies/Microsoft.UI.Xaml.2.8.appx"
 }
 if (-Not (Test-Path -Path "./ps1/winget-dependencies\Microsoft.VCLibs.140.00.UWPDesktop_14.0.33728.0_x64__8wekyb3d8bbwe.appx"))
 {
-    Invoke-WebRequest -Uri "http://ccje-websrv1/repositorio/Microsoft.VCLibs.140.00.UWPDesktop_14.0.33728.0_x64__8wekyb3d8bbwe.Appx" -OutFile "./ps1/winget-dependencies/Microsoft.VCLibs.140.00.UWPDesktop_14.0.33728.0_x64__8wekyb3d8bbwe.appx"
+    Invoke-WebRequest -Uri "http://ccje-web/lab/Microsoft.VCLibs.140.00.UWPDesktop_14.0.33728.0_x64__8wekyb3d8bbwe.Appx" -OutFile "./ps1/winget-dependencies/Microsoft.VCLibs.140.00.UWPDesktop_14.0.33728.0_x64__8wekyb3d8bbwe.appx"
 }

85e9d0722e970a700704ac7a1898ab159963f3b0
nicholas <nicholas.pinto@ufes.br>
2026-01-07 09:50:40 -0300

    7/1 improvements

--- ps1/ci-bizagi.ps1
+++ ps1/ci-bizagi.ps1
@@ -1,17 +1,6 @@
 Set-SmbClientConfiguration -EnableInsecureGuestLogons $true -Force
 Set-SmbClientConfiguration -RequireSecuritySignature $false -Force
 
-$source_path = "\\ccje-smb2\lc_guest\"
-$target_path = "C:\Users\Aluno_CCJE\Desktop\inst_manual"
-$filename1 = "BizagiModelerSetup_4.1.0002.exe"
+$source_path = "\\ccje-smb2\lc_guest\BizagiModeler.msi"
 
-New-Item -Path $target_path -ItemType Directory -Force
-
-try 
-{
-    robocopy $source_path $target_path $filename1 /eta
-}
-catch 
-{
-    Write-Host "File copy error"
-}
+& msiexec.exe /qn /i "$source_path" Lang = pt | Write-Verbose
--- ps1/ci-graphdb.ps1
+++ ps1/ci-graphdb.ps1
@@ -1,19 +1,6 @@
 Set-SmbClientConfiguration -EnableInsecureGuestLogons $true -Force
 Set-SmbClientConfiguration -RequireSecuritySignature $false -Force
 
-$source_path = "\\ccje-smb2\lc_guest\"
-$target_path = "C:\Users\Aluno_CCJE\Desktop\inst_manual"
-$filename1 = "GraphDB_Desktop-10.1.4.msi"
+$source_path = "\\ccje-smb2\lc_guest\GraphDB_Desktop-10.1.4.msi"
 
-New-Item -Path $target_path -ItemType Directory -Force
-
-try 
-{
-    robocopy $source_path $target_path $filename1 /eta
-}
-catch 
-{
-    Write-Host "File copy error"
-}
-
-msiexec /qn /i "$target_path\$filename1" ALLUSERS=1
+msiexec /qn /i "$source_path" ALLUSERS=1

35e26a72b988df2d9107c15ebe09d2f86c016299
nicholas <nicholas.pinto@ufes.br>
2026-01-07 10:57:28 -0300

    fix data typo

--- ps1/ci-bizagi.ps1
+++ ps1/ci-bizagi.ps1
@@ -3,4 +3,4 @@ Set-SmbClientConfiguration -RequireSecuritySignature $false -Force
 
 $source_path = "\\ccje-smb2\lc_guest\BizagiModeler.msi"
 
-& msiexec.exe /qn /i "$source_path" Lang = pt | Write-Verbose
+& msiexec.exe /i "$source_path" /qn Lang=pt ISSETUPDRIVEN=1 | Write-Verbose
